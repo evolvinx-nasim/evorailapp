aaa51e1966ff464554749fa1a7b04b8567340bd1
Evolvinx Developer <nasim@evolvinx.com>
2013-07-07 13:16:45 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evorailapp::Application.config.secret_token = 'bba647bee4a2bebedc3f028d38a33c78f90258449a2f0b4585fa9a309cfb2e1bcbbdbfb6f7457a8374b755642d749948dddb08076c251cc6f19baf2602d7c0bb'
